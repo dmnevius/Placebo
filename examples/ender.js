e7e0989d228a0b1787ebfd2affe14a2bdb16471e
Daniel Nevius <dmneviusthemouse@gmail.com>
2016-01-30 23:11:47 -0500

    Placebo 1.0.10

--- examples/ender.js
+++ examples/ender.js
@@ -1,13 +1,10 @@
 var $ = window.$,
     require = window.require,
     placebo = window.placebo,
-    placebo2 = require('placebo-js');
+    placebo2 = require('placebo');
 
 // Global object
 placebo('div#foo').place();
-
-// Top-level ender object
-$.placebo('div#bar').place();
 
 // Require()
 placebo2('div#baz').place();
