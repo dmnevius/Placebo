145b6d7ac9bf9bd9e8d83294b3e7caff428630b8
Daniel Nevius <dmneviusthemouse@gmail.com>
2016-03-24 16:54:51 -0400

    Removed accidentally published vendor libraries

--- examples/node_modules/placebo-js/ender.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-    'use strict';
-    var placebo = require('placebo-js');
-    $.ender(placebo);
-    $.ender({
-        placebo: placebo
-    });
-}(ender));
--- examples/node_modules/placebo-js/examples/requirejs.js
+++
@@ -1,7 +0,0 @@
-// Use placebo with RequireJS (or any other AMD module library) http://requirejs.org
-var require = window.require;
-
-require(['../placebo'], function (placebo) {
-    'use strict';
-    placebo('h1#title').text("Placebo &amp; RequireJS").place();
-});
